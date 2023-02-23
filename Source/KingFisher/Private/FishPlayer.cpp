952dc69c6f86278ba95ef2a5aa667b22e900dd41
IkHyunn <120433846+IkHyunn@users.noreply.github.com>
2023-02-23 20:24:56 +0900

    ..

--- Source/KingFisher/Private/FishPlayer.cpp
+++ Source/KingFisher/Private/FishPlayer.cpp
@@ -131,19 +131,6 @@ AFishPlayer::AFishPlayer()
 	{
 		castSound = tempcastingsound.Object;
 	}
-
-// 	ConstructorHelpers::FObjectFinder<UAudioComponent>tempreelsound(TEXT("/Script/Engine.SoundWave'/Game/Resources/Sound/FOL_Reeling.FOL_Reeling'"));
-// 	if (tempreelsound.Succeeded())
-// 	{
-// 		reelingSound = tempreelsound.Object;
-// 	}
-
-	//¹°°í±â UI
-	ConstructorHelpers::FClassFinder<AfishWidgetActor> tempfishUI (TEXT("/Script/Engine.Blueprint'/Game/BluePrints/Widget/BP_fishwidgetActor.BP_fishwidgetActor_C'"));
-	if (tempfishUI.Succeeded())
-	{
-		fish_Ui=tempfishUI.Class;
-	}
 }
 
 // Called when the game starts or when spawned
